68cd76ccf64fdae4544694b85657f3454dd00474
sehee-jj <aselysh@naver.com>
2025-02-07 12:30:57 +0900

    Initial commit

---
+++ Source/Quest08/Quest08.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Quest08.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Quest08, "Quest08" );
